716b434b72f2cc36a35d3005cd1949774c202f5c
João Fernando <ra120109@uem.br>
2024-07-24 20:58:27 -0300

    cache

--- cache.py
+++ cache.py
@@ -1,19 +1,41 @@
 import random
+import queue
+import ram
 
 class mem_cache:
     def __init__(self):
         self.tamanho = 5
         self.linha = [None] * self.tamanho
         self.tag =[None] * self.tamanho
+        self.fifo = queue.Queue(self.tamanho)
 
-    def mapeia_cache(self, ram, pos):
-        
-        cheia = False
+    def notCheia(self):
+        notCheia = False
+        i = 0
 
-        while not cheia:
-            indice = random.randint(0, 4)
-
-            if self.linha[indice] == None:
-                self.linha[indice] = ram.dados[pos]
+        while not notCheia and i < self.tamanho:
+            if self.linha[i] == None:
+                notCheia = True
             else:
-                cheia = True
+                i += 1
+
+        return notCheia
+
+    def mapeia_cache(self, RAM : ram.mem_ram , pos):
+        notCheia = self.notCheia()
+
+        solicitou = False
+
+        if notCheia:
+            while not solicitou:
+                indice = random.randint(0, 4)
+
+                if self.linha[indice] == None:
+
+                    self.linha[indice] = RAM.dados[pos]
+                    self.fifo.put(indice)
+
+                    solicitou = True
+        else:
+            ##substituição
+            return
